3199ae5eae659e1474d858701d4d90b8fe2650b4
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2021-03-05 15:10:03 +0530

    Create Sum OR Difference.C

---
+++ Sum OR Difference/Sum OR Difference.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int num1,num2;
+   scanf("%d",&num1);
+   scanf("%d",&num2);
+   if(num1>num2)
+   {
+      printf("%d",(num1-num2));
+   }
+   else
+   {
+      printf("%d",(num1+num2));
+   }
+	return 0;
+}
+
