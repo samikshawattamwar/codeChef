2d4745c306976b090a4d40a9a8668e53393287bb
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2021-03-16 11:56:51 +0530

    Create Area OR Perimeter.py

---
+++ Area OR Perimeter/Area OR Perimeter.py
@@ -0,0 +1,16 @@
+# cook your dish here
+l = int(input())
+b = int(input())
+
+area = l*b
+peri = 2*(l+b)
+
+if (area>peri):
+   print("Area")
+   print(area)
+elif (area>peri):
+   print("Peri")
+   print(peri)
+else:
+   print("Eq")
+   print(peri)
